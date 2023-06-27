925126d604863a15d2acaab634029625a914cda8
2chaelim <charry8540@gmail.com>
2023-06-27 11:28:26 +0900

    homepage url 수정

--- src/index.js
+++ src/index.js
@@ -2,23 +2,23 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 import App from "./App";
+import store from "./store/store.js";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
 
 import "./css/my_reset.css";
 import "./css/index.css";
 import "./css/App.css";
-import store from "./store/store.js";
-import { Provider } from "react-redux";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <BrowserRouter>
+    <BrowserRouter basename="/Pro4_MapsMart/">
+      <Provider store={store}>
         <App />
-      </BrowserRouter>
-    </Provider>
+      </Provider>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
